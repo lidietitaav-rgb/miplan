371d1360c5ba9d60b60b51b41404de75d5128190
lidietitaav-rgb <lidietitaav@gmail.com>
2026-08-19 17:37:46 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'miplan-v2';
+const CACHE = 'miplan-v3';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
